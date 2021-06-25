6df062417f9e7ceab26227d6ea82763963225bc3
Alpha389 <75525117+theshivambedi@users.noreply.github.com>
2021-06-26 00:03:24 +0530

    Add files via upload

---
+++ Bmicalci(2).py
@@ -0,0 +1,5 @@
+# How to calculate BMI in python
+height = float(input("enter your height in m : \n"))
+weight = float(input("enter your weight in kgs : \n"))
+bmi = int(weight/(height**2))
+print(f"your BMI is {bmi}")
