33dba4001b629fdd31221d785693c7bbfa2b6d38
Alpha389 <75525117+theshivambedi@users.noreply.github.com>
2021-06-25 19:08:38 +0530

    Add files via upload

---
+++ addtwonumbers(1).py
@@ -0,0 +1,8 @@
+# project 1
+#ask user for a two digit number and add it 
+
+number = (input("type a two digit number \n"))
+first_digit = number[0]
+second_digit = number[1]
+result = int(first_digit) + int(second_digit)
+print(result)
